2e5ae8d69f3876d09b68643697daeccf3bb2dffc
joc6 <joc6@L7BWTMK106-86.AD.PSU.EDU>
2021-10-13 17:48:03 -0400

    Added Controller ad center panel, need updates for both

--- src/main/java/View/CenterPanel.java
+++ src/main/java/View/CenterPanel.java
@@ -15,69 +15,58 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 
 public class CenterPanel extends JPanel {
-    // attributes
-    ArrayList<JButton> headers = new ArrayList<JButton>();
-    ArrayList<JButton> rowData = new ArrayList<JButton>();
-    Border border = new LineBorder(Color.BLUE, 9);
-
-    public CenterPanel() {
+    private JButton player1;
+    private JButton player2;
+    private JButton update;
+    private JButton title;
+    private JTextField player1Name;
+    private JTextField player2Name;
+    Border border = new LineBorder(Color.BLACK, 3);
+    
+    public CenterPanel()
+    {
         super();
-    }
- 
-    public void setupCenterPanel(int rows, int cols) {
-        setLayout(new GridLayout(rows + 1, cols)); //change according to difficulty setting
-        // create header labels and add to panel
-        for(int c = 0; c < cols; c++) {
-            JButton label = new JButton("Drop");
-            label.setHorizontalAlignment(JLabel.CENTER);
-            label.setBackground(Color.LIGHT_GRAY);
-            label.setBorder(BorderFactory.createLineBorder(Color.black));
-            headers.add(label);
-            add(headers.get(c));
-        }
-        // create row data buttons and add to panel
-        int r = 1;
-        for(int i = 0; i < rows; i++) {
-            for(int c = 0; c < cols; c++) {
-                JButton btn = new JButton();
-                btn.setBackground(Color.LIGHT_GRAY);
-                rowData.add(btn);
-                add(btn);
-                btn.setBorder(border);
-                r++;
-            }
-        }        
+        setLayout(new GridLayout());
+        title = new JButton("Connect Four");
+        player1 = new JButton();
+        player2 = new JButton();
+        update = new JButton("Update");
+        player1Name = new JTextField("Enter Player 1");
+        player2Name = new JTextField("Enter Player 2");
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setBackground(Color.BLACK);
+        title.setForeground(Color.WHITE);
+        player1.setBorder(border);
+        player2.setBorder(border);
+        add(player1);
+        add(player1Name);
+        add(title);
+        add(player2Name);
+        add(player2);
+        add(update);
         validate();
         repaint();
     }
     
-    public void panelUpdate(ArrayList<ArrayList<String>> lines, 
-            ArrayList<String> heads) {
-        for (int h = 0; h < heads.size(); h++) {
-            headers.get(h).setText(heads.get(h));
-        }
-        int b = 0;
-        for (int d = 0; d < lines.size() - 1; d++) {
-            ArrayList<String> data = lines.get(d);
-            for (int i = 0; i < data.size(); i++) {
-                rowData.get(b).setText(data.get(i));
-                b++;
-            } 
-        }
-        validate();
-        repaint();
-    }    
-    public ArrayList<JButton> getHeaders() {
-        return headers;
-    }
-
-    public ArrayList<JButton> getRowData() {
-        return rowData;
+    
+    public JTextField getName1()
+    {
+        return player1Name;
     }
     
+    public JTextField getName2()
+    {
+        return player2Name;
+    }
+    
+    public JButton getUpdate()
+    {
+        return update;
+    }
 }
