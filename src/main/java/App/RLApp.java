616d4f5d3bc1a2d95ba62a36bc85592b1501d64a
Marsupius <62824473+Marsupius@users.noreply.github.com>
2021-11-02 12:47:42 -0400

    Rename model class to RLApp

--- src/main/java/App/RLApp.java
+++ src/main/java/App/RLApp.java
@@ -13,11 +13,12 @@ import com.mycompany.Model.Model;
  *
  * @author joc6
  */
-public class RuneLiteApp
+public class RLApp
 {
     public static void main (String[] args){
         View view = new View();
         Model model = new Model();
         Controller controller = new Controller(model, view);
+        System.out.println("It is open");
     }
 }
